789fae55a7c46e51664010d22835db70812319b0
mmagnuski <mmagnuski@swps.edu.pl>
2021-10-22 17:17:05 +0200

    TST: add test for crop

---
+++ pylabianca/test/test_spikes.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pylabianca as pln
+
+
+def create_fake_spikes():
+    times = [[-0.3, -0.1, 0.025, 0.11, 0.22, 0.25, 0.4,
+            -0.08, 0.12, 0.14, 0.19, 0.23, 0.32],
+            [-0.22, -0.13, -0.03, 0.08, 0.16, 0.33, -0.2,
+            -0.08, 0.035, 0.148, 0.32]]
+    trials = [[0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1]]
+    spk = pln.SpikeEpochs(times, trials)
+    return spk
+
+
+def test_crop():
+    spk = create_fake_spikes()
+    spk.crop(tmin=0.1)
+
+    assert (spk.time[0] == np.array(
+        [0.11, 0.22, 0.25, 0.4, 0.12, 0.14, 0.19, 0.23, 0.32])).all()
+    assert (spk.time[1] == np.array([0.16, 0.33, 0.148, 0.32])).all()
+    assert (spk.trial[0] == np.array([0, 0, 0, 0, 1, 1, 1, 1, 1])).all()
+    assert (spk.trial[1] == np.array([0, 0, 1, 1])).all()
