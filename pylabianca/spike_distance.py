45a12e06d46a76dc89f0c589b898641408611b81
mmagnuski <mmagnuski@swps.edu.pl>
2023-09-28 18:07:12 +0200

    DEV, FIX: move co_spikes code to spike_distance

--- pylabianca/spike_distance.py
+++ pylabianca/spike_distance.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from . import utils, viz
 from .utils import (_deal_with_picks, _turn_spike_rate_to_xarray,
                     _symmetric_window_samples, _gauss_kernel_samples,
                     spike_centered_windows)
@@ -112,7 +113,6 @@ def spike_xcorr_density(spk, cell_idx, picks=None, sfreq=500, winlen=0.1,
     # calculate spike density
     picks = _deal_with_picks(spk, picks)
     tms, cnt = _spike_density(spk, picks=picks, sfreq=sfreq, kernel=kernel)
-    cnt = cnt.transpose((1, 0, 2))
 
     # cut out spike-centered windows
     windows = spike_centered_windows(
@@ -130,8 +130,11 @@ def spike_xcorr_density(spk, cell_idx, picks=None, sfreq=500, winlen=0.1,
     time = np.arange(win_diff[0], win_diff[1] + 0.01 * t_per_smp,
                      step=t_per_smp)
     cell_names = [spk.cell_names[idx] for idx in picks]
-    xcorr = _turn_spike_rate_to_xarray(time, windows, spk, tri=windows.trial,
-                                       cell_names=cell_names)
+    windows = windows.transpose('channel', 'spike', 'time')
+    xcorr = _turn_spike_rate_to_xarray(
+        time, windows, spk, tri=windows.trial.values,
+        cell_names=cell_names
+    )
 
     return xcorr
 
@@ -189,6 +192,100 @@ def spike_xcorr_elephant(spk, cell_idx1, cell_idx2, sfreq=500, winlen=0.1,
         lags, cch_list[None, :], spk, cell_names=[cell_name],
         copy_cellinfo=False)
     return cch
+
+
+def xcorr(x, y, maxlag=None):
+    '''Compute un-normalized cross-correlation for required max lag.
+
+    This is equivalent to convolution but without reversing the kernel.
+
+    Parameters
+    ----------
+    x : numpy.ndarray
+        First 1d array to crosscorrelate.
+    y : numpy.ndarray
+        Second 1d array to crosscorrelate.
+    maxlab : int
+        Maximum lag in samples.
+
+    Returns
+    -------
+    lags : numpy.ndarray
+        1d array of lags (in samples).
+    corr : numpy.ndarray
+        1d array of crosscorrelations.
+    '''
+    from scipy.signal import correlate, correlation_lags
+
+    corr = correlate(x, y)
+    lags = correlation_lags(len(x), len(y))
+
+    if maxlag is not None:
+        from borsar.utils import find_range
+
+        rng = find_range(lags, [-maxlag, maxlag])
+        corr, lags = corr[rng], lags[rng]
+
+    return lags, corr
+
+
+def shuffled_spike_xcorr(spk, cell_idx1, cell_idx2, sfreq=500,
+                         gauss_winlen=0.025, max_lag=0.1, pbar=True,
+                         n_shuffles=1_000):
+    '''FIXME'''
+    from scipy.signal import correlate
+    from .viz import check_modify_progressbar
+
+    pbar = check_modify_progressbar(pbar, total=n_shuffles)
+    max_lag_smp = int(np.round(sfreq * max_lag))
+
+    time, bin_spk = spk.to_raw(picks=[cell_idx1, cell_idx2], sfreq=sfreq)
+    n_trials, n_cells, n_times = bin_spk.shape
+
+    # add maxlag separation between trials
+    add_sep = np.zeros(shape=(n_trials, n_cells, max_lag_smp))
+    bin_spk = np.concatenate([bin_spk, add_sep], axis=2)
+    n_times = bin_spk.shape[-1]
+
+    win, _ = _symmetric_window_samples(gauss_winlen, sfreq=sfreq)
+    sd_smp = int(np.round((sfreq * gauss_winlen) / 6))
+    gauss = _gauss_kernel_samples(win, sd_smp)
+
+    # unroll to cells x (trials * times)
+    concat = bin_spk.transpose(1, 0, 2).reshape(n_cells, n_trials * n_times)
+
+    # turn spikes into continuous signal
+    cnt = correlate(concat, gauss[None, :], mode='same')
+    lags, corr = xcorr(cnt[0], cnt[1], maxlag=max_lag_smp)
+    lags = lags / sfreq
+
+    if n_shuffles > 0:
+        n_lags = len(lags)
+        corr_shuffled = np.zeros(shape=(n_shuffles, n_lags))
+
+        cnt1 = cnt[0]
+        # shuffle trials n_times
+        for shuffle_idx in range(n_shuffles):
+            tri = np.arange(n_trials)
+            np.random.shuffle(tri)
+
+            # unroll to cells x (trials * times)
+            bin_spk_shuffled = bin_spk[tri, 1]
+            concat_shuffled = bin_spk_shuffled.reshape(n_trials * n_times)
+
+            # turn spikes into continuous signal
+            cnt_shuffled = correlate(concat_shuffled, gauss, mode='same')
+            _, this_corr = xcorr(cnt1, cnt_shuffled, maxlag=max_lag_smp)
+            corr_shuffled[shuffle_idx] = this_corr
+            pbar.update(1)
+
+        avg_shfl = corr_shuffled.mean(axis=0)
+        std_shfl = corr_shuffled.std(axis=0)
+    else:
+        avg_shfl = 0
+        std_shfl = 1
+
+    return lags, (corr - avg_shfl) / std_shfl
 
 
 def find_coincidence_clusters(similarity, threshold=0.3):
