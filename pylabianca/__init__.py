a30df26e90c04e2aa34d6fedb836ea3b7a6cdcad
mmagnuski <mmagnuski@swps.edu.pl>
2023-09-29 12:57:18 +0200

    DEV: removed co_spikes.py after it was moved to spike_distance.py

--- pylabianca/__init__.py
+++ pylabianca/__init__.py
@@ -4,4 +4,3 @@ from . import (io, utils, co_spikes, decoding, localize, selectivity, spikes,
 from .viz import plot_spike_rate, plot_raster, plot_spikes
 from .spikes import SpikeEpochs, Spikes
 from .selectivity import depth_of_selectivity, explained_variance
-from .co_spikes import shuffled_spike_xcorr
