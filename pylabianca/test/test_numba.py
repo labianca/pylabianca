a3e7f0c8511539a8eb578f47cd5ca78e80e61d2e
mmagnuski <mmagnuski@swps.edu.pl>
2024-05-30 11:46:12 +0200

    ENH: add numba _monotonic_find_first and _select_spikes

--- pylabianca/test/test_numba.py
+++ pylabianca/test/test_numba.py
@@ -14,3 +14,19 @@ def test_monotonic_unique_counts():
 
     assert (out[0] == np.array([ 2,  5,  8,  9, 10], dtype='int64')).all()
     assert (out[1] == np.array([3, 5, 2, 8, 4], dtype='int64')).all()
+
+
+def test_numba_select_spikes():
+    from pylabianca._numba import _select_spikes_numba
+
+    def select_spikes(spikes, trials, tri_sel):
+        msk = np.in1d(trials, tri_sel)
+        return spikes[msk]
+
+    spikes = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    trials = np.array([1, 1, 1, 2, 2, 2, 3, 3, 3, 4])
+    tri_sel = np.array([1, 3, 4])
+
+    out = _select_spikes_numba(spikes, trials, tri_sel)
+    expected = select_spikes(spikes, trials, tri_sel)
+    assert (out == expected).all()
