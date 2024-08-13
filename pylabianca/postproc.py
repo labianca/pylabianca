a91450c34dd6fed2c6bb67e4833238050dea50a1
mmagnuski <mmagnuski@swps.edu.pl>
2024-08-13 22:07:20 +0200

    STY: fix variable names and other minor things

--- pylabianca/postproc.py
+++ pylabianca/postproc.py
@@ -46,11 +46,11 @@ def find_coincidence_clusters(similarity, threshold=0.3):
     adj = similarity >= threshold
     suspicious = adj.any(axis=0) | adj.any(axis=1)
     suspicious_idx = np.where(suspicious)[0]
-    adj_susp = adj[suspicious_idx[:, None], suspicious_idx[None, :]]
+    adj_suspicious = adj[suspicious_idx[:, None], suspicious_idx[None, :]]
 
     fake_signal = np.ones(len(suspicious_idx))
     clusters, counts = borsar.cluster.find_clusters(
-        fake_signal, 0.5, adjacency=adj_susp, backend='mne')
+        fake_signal, 0.5, adjacency=adj_suspicious, backend='mne')
     return suspicious_idx, clusters, counts
 
 
@@ -79,7 +79,7 @@ def plot_high_similarity_cluster(spk, similarity, clusters, suspicious_idx,
         not colored in red.
     figsize : tuple
         Two-element tuple specifying the size of the figure (in inches, as
-        matploltib likes it).
+        matplotlib likes it).
 
     Returns
     -------
@@ -91,7 +91,7 @@ def plot_high_similarity_cluster(spk, similarity, clusters, suspicious_idx,
     idxs = suspicious_idx[clusters[cluster_idx]]
 
     n_cells = len(idxs)
-    simil_part = similarity[idxs[:, None], idxs[None, :]]
+    similarity_part = similarity[idxs[:, None], idxs[None, :]]
 
     fig, ax = plt.subplots(nrows=n_cells + 1, ncols=n_cells + 1,
                            figsize=figsize)
@@ -122,14 +122,14 @@ def plot_high_similarity_cluster(spk, similarity, clusters, suspicious_idx,
         this_ax.set_yticks([])
 
     ax[0, 0].axis(False)
-    max_val = simil_part.max()
+    max_val = similarity_part.max()
     for row_idx in range(n_cells):
         for col_idx in range(n_cells):
             if row_idx == col_idx:
                 ax[row_idx + 1, col_idx + 1].axis(False)
                 continue
 
-            value = simil_part[row_idx, col_idx]
+            value = similarity_part[row_idx, col_idx]
             val_perc = value / max_val
             txt_col = 'black' if val_perc > 0.5 else 'white'
             ax[row_idx + 1, col_idx + 1].text(0.5, 0.5, f'{value:0.3f}',
@@ -212,8 +212,8 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
 
 
     if weights is None:
-        weights = {'isi': 0.15, 'nspikes': 0.35, 'snr': 0.0,
-                'std': 0.0, 'dns': 0.5}
+        weights = {'isi': 0.15, 'n_spikes': 0.35, 'snr': 0.0,
+                   'std': 0.0, 'dns': 0.5}
 
     save_fig = fig_dir is not None
     if save_fig:
@@ -269,19 +269,20 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
     n_cells = len(spk)
     spk_epo = spk.to_epochs()
 
-    measure_names = ['nspikes', 'snr', 'isi', 'std', 'dns']
+    measure_names = ['n_spikes', 'snr', 'isi', 'std', 'dns']
     measures = {name: np.zeros(n_cells, dtype='float')
                 for name in measure_names}
-    measures['nspikes'] = n_spikes
+    measures['n_spikes'] = n_spikes
 
     for ix in tqdm(range(n_cells)):
         # SNR
-        waveform_ampl = np.mean(spk.waveform[ix][:, alignment_sample], axis=0)
+        waveform_amplitude = np.mean(
+            spk.waveform[ix][:, alignment_sample], axis=0)
         waveform_std = np.std(spk.waveform[ix][:, alignment_sample], axis=0)
-        measures['snr'][ix] = np.abs(waveform_ampl) / waveform_std
+        measures['snr'][ix] = np.abs(waveform_amplitude) / waveform_std
 
         # ISI
-        isi_val = np.diff(spk_epo.time[ix])
+        isi_val = np.diff(spk_epo.time[ix])s
         prop_below_3ms = (isi_val < 0.003).mean()
         measures['isi'][ix] = prop_below_3ms
 
@@ -304,13 +305,13 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
 
     fname = 'coincidence_per_pack.hdf5'
     files = os.listdir(spike_data_dir)
-    has_simil = fname in files
+    has_similarity = fname in files
 
     group_idx = 0
-    df_columns=['pack', 'group', 'subgroup', 'channel', 'cluster', 'nspikes',
+    df_columns=['pack', 'group', 'subgroup', 'channel', 'cluster', 'n_spikes',
                 'snr', 'isi', 'std', 'dns', 'drop']
 
-    if not has_simil:
+    if not has_similarity:
         # turn to function
         print('Calculating similarity matrix, this can take a few minutes...')
         similarity_per_pack = list()
@@ -335,21 +336,21 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
     # drop_perc = drop.copy()
     df_list = list()
 
-    reordered_names = ['nspikes', 'snr', 'dns', 'isi', 'std']
+    reordered_names = ['n_spikes', 'snr', 'dns', 'isi', 'std']
     weights_sel = np.array(
         [weights[name] for name in reordered_names]
     )[None, :]
 
-    nodrop_simil = list()
-    for pack_idx, (pack_units_idx, simil) in enumerate(
+    no_drop_similarity = list()
+    for pack_idx, (pack_units_idx, similarity) in enumerate(
             zip(units_in_pack, similarity_per_pack)):
 
-        if simil is None:
+        if similarity is None:
             continue
 
         spk_pack = spk.copy().pick_cells(pack_units_idx)
         suspicious_idx, clusters, counts = find_coincidence_clusters(
-                simil, threshold=coincidence_threshold)
+                similarity, threshold=coincidence_threshold)
 
         # detect likely REF clusters and select units
         # -------------------------------------------
@@ -357,8 +358,8 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
 
         for cluster_idx in check_clst_idx:
             print(f'Processing pack {pack_idx}, cluster {cluster_idx}...')  # x
-            incluster_cell_indices = suspicious_idx[clusters[cluster_idx]]
-            cell_idx = pack_units_idx[incluster_cell_indices]
+            in_cluster_cell_indices = suspicious_idx[clusters[cluster_idx]]
+            cell_idx = pack_units_idx[in_cluster_cell_indices]
             channels = spk.cellinfo.loc[cell_idx, 'channel'].unique()
             if len(channels) < min_channels:
                 continue
@@ -369,7 +370,7 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
             # -----------------------------
             measures_sel = np.stack(
                 [measures[name][cell_idx]
-                for name in ['nspikes', 'snr', 'dns']
+                for name in ['n_spikes', 'snr', 'dns']
                 ] +
                 [(1 - measures_prc[name][cell_idx])
                 for name in ['isi', 'std']],
@@ -377,7 +378,7 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
             )
             measures_perc_sel = np.stack(
                 [measures_prc[name][cell_idx]
-                for name in ['nspikes', 'snr', 'dns']
+                for name in ['n_spikes', 'snr', 'dns']
                 ] +
                 [(1 - measures_prc[name][cell_idx])
                 for name in ['isi', 'std']]
@@ -411,42 +412,45 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
             for ix, measure_name in enumerate(reordered_names):
                 this_df.loc[:, measure_name] = measures_sel[:, ix]
 
-            incl_idx = np.ix_(incluster_cell_indices, incluster_cell_indices)
-            incluster_simil = simil[incl_idx]
+            incl_idx = np.ix_(in_cluster_cell_indices, in_cluster_cell_indices)
+            in_cluster_similarity = similarity[incl_idx]
 
             # iteratively drop according to score_ranks
             subgroup_id = 1
-            drop_msk = np.zeros(incluster_simil.shape[0], dtype='bool')
-            left_simil = incluster_simil[~drop_msk][:, ~drop_msk]
+            drop_msk = np.zeros(in_cluster_similarity.shape[0], dtype='bool')
+            left_similarity = in_cluster_similarity[~drop_msk][:, ~drop_msk]
 
-            while (left_simil > coincidence_threshold).any():
+            while (left_similarity > coincidence_threshold).any():
                 this_df.loc[~drop_msk, 'subgroup'] = np.nan
                 any_left = this_df.subgroup.isna().any()
                 while any_left:
                     subgroup_idx = np.where(this_df.subgroup.isna())[0]
                     best_idx = score_ranks[subgroup_idx].argmax()
-                    notdrop_idx = subgroup_idx[best_idx]
+                    not_drop_idx = subgroup_idx[best_idx]
 
                     other_subgroup_idx = np.delete(subgroup_idx, best_idx)
-                    other_related = incluster_simil[other_subgroup_idx, notdrop_idx]
+                    other_related = in_cluster_similarity[
+                        other_subgroup_idx, not_drop_idx]
                     drop = other_related > coincidence_threshold
-                    other_related = incluster_simil[notdrop_idx, other_subgroup_idx]
+                    other_related = in_cluster_similarity[
+                        not_drop_idx, other_subgroup_idx]
                     drop = drop | (other_related > coincidence_threshold)
                     drop_idx = other_subgroup_idx[drop]
                     this_df.loc[drop_idx, 'subgroup'] = subgroup_id
                     this_df.loc[drop_idx, 'drop'] = True
 
-                    this_df.loc[notdrop_idx, 'subgroup'] = subgroup_id
-                    this_df.loc[notdrop_idx, 'drop'] = False
+                    this_df.loc[not_drop_idx, 'subgroup'] = subgroup_id
+                    this_df.loc[not_drop_idx, 'drop'] = False
 
                     # select the best unit
                     any_left = this_df.subgroup.isna().any()
                     subgroup_id += 1
 
-                # aggregate nodrop simil
+                # aggregate no_drop similarity
                 drop_msk = this_df.loc[:, 'drop'].values.astype('bool')
-                left_simil = incluster_simil[~drop_msk][:, ~drop_msk]
-            nodrop_simil.append(left_simil)
+                left_similarity = in_cluster_similarity[
+                    ~drop_msk][:, ~drop_msk]
+            no_drop_similarity.append(left_similarity)
 
             df_list.append(this_df)
 
@@ -456,7 +460,7 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
                 fname_base = f'pack_{pack_idx:02g}_cluster_{cluster_idx:02g}'
                 fname = fname_base + '_01_coincid.png'
                 fig = plot_high_similarity_cluster(
-                    spk_pack, simil, clusters, suspicious_idx,
+                    spk_pack, similarity, clusters, suspicious_idx,
                     cluster_idx=cluster_idx
                 )
                 fig.savefig(op.join(fig_dir, fname), dpi=300)
@@ -474,7 +478,7 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
                 plt.close(fig)
 
                 # plot after if more than one subgroup
-                if left_simil.shape[0] > 1:
+                if left_similarity.shape[0] > 1:
                     fname = fname_base + '_02_coincid_after.png'
                     these_clusters = [clst.copy() for clst in clusters]
                     this_clst_msk = these_clusters[cluster_idx]
@@ -482,7 +486,7 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
                     this_clst_msk[this_clst_idx[drop_msk]] = False
 
                     fig = plot_high_similarity_cluster(
-                        spk_pack, simil, these_clusters, suspicious_idx,
+                        spk_pack, similarity, these_clusters, suspicious_idx,
                         cluster_idx=cluster_idx)
                     fig.savefig(op.join(fig_dir, fname), dpi=300)
                     plt.close(fig)
@@ -500,7 +504,7 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
                 fname_base = f'pack_{pack_idx:02g}_cluster_{cluster_idx:02g}'
                 fname = fname_base + '_01_coincid.png'
                 fig = plot_high_similarity_cluster(
-                    spk_pack, simil, clusters, suspicious_idx,
+                    spk_pack, similarity, clusters, suspicious_idx,
                     cluster_idx=cluster_idx)
                 fig.savefig(op.join(fig_dir, 'ignored_clusters', fname),
                             dpi=300)
@@ -509,7 +513,7 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
     print('Saving dataframe to figures location...')
     df = pd.concat(df_list).reset_index(drop=True)
     df = df.loc[:, df_columns]
-    df.loc[:, 'nspikes'] = df.nspikes.astype('int')
+    df.loc[:, 'n_spikes'] = df.n_spikes.astype('int')
     df.to_csv(op.join(fig_dir, 'table.tsv'), sep='\t')
 
     print('All done.')
@@ -541,26 +545,26 @@ def mark_duplicates(spike_data_dir, first_channel, fig_dir=None,
 # plot_edge_threshold = 0.15 # coincidence_threshold
 
 # pack_units_idx = units_in_pack[pack_idx]
-# simil = similarity_per_pack[pack_idx]
+# similarity = similarity_per_pack[pack_idx]
 
 # spk_pack = spk.copy().pick_cells(pack_units_idx)
 # suspicious_idx, clusters, counts = (
 #     pln.spike_distance.find_coincidence_clusters(
-#         simil,
+#         similarity,
 #         threshold=coincidence_threshold
 #     )
 # )
 
 # idx = suspicious_idx[clusters[cluster_idx]]
-# simil_clst = simil[idx][:, idx]
+# similarity_clst = similarity[idx][:, idx]
 
 # # construct graph
 # G = nx.DiGraph()
 
-# n_units = simil_clst.shape[0]
+# n_units = similarity_clst.shape[0]
 # for idx1 in range(n_units):
 #     for idx2 in range(n_units):
-#         fromto = simil_clst[idx1, idx2]
+#         fromto = similarity_clst[idx1, idx2]
 #         if fromto > plot_edge_threshold:
 #             G.add_edges_from([(idx1, idx2)], weight=fromto)
 
