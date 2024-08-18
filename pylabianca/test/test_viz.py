5ad46e2a7947be926ab8e72939640a9d11e2f5c3
mmagnuski <mmagnuski@swps.edu.pl>
2024-08-19 00:30:13 +0200

    TST:cleanup

--- pylabianca/test/test_viz.py
+++ pylabianca/test/test_viz.py
@@ -22,11 +22,11 @@ def test_plot_shaded():
         data, dims=['cell', 'trial', 'time'],
         coords={'time': times})
 
-    # %%
     msg_to_match = 'DataArray contains too many dimensions'
     with pytest.raises(ValueError, match=msg_to_match):
         pln.plot_shaded(xarr1)
-        ax = pln.plot_shaded(xarr1, groupby='cell')
+
+    ax = pln.plot_shaded(xarr1, groupby='cell')
 
     # has n_cells lines, each n_times long
     assert len(ax.lines) == n_cells
@@ -39,11 +39,10 @@ def test_plot_shaded():
 
     # LATER: check that there is legend present with correct labels
 
-    # %%
     # groupby dim should be removed from auto-finding reduce_dim
     ax1 = pln.plot_shaded(xarr1, groupby='trial', legend=False)
-
-    ax2 = pln.plot_shaded(xarr1, groupby='trial', reduce_dim='cell', legend=False)
+    ax2 = pln.plot_shaded(xarr1, groupby='trial', reduce_dim='cell',
+                          legend=False)
 
     assert len(ax1.lines) == len(ax2.lines) == n_trials
 
@@ -53,8 +52,7 @@ def test_plot_shaded():
 
 
 def test_plot_raster():
-    spk = create_random_spikes(n_cells=1)
-
+    spk = pln.utils.create_random_spikes(n_cells=1)
     ax = pln.viz.plot_raster(spk)
 
     for idx, tri in enumerate(np.unique(spk.trial[0])):
