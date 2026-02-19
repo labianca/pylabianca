798c0302ab3a2061d4331a85aa3d32ebea6e9766
mmagnuski <mmagnuski@swps.edu.pl>
2026-02-19 15:30:47 +0100

    simplify futurewarning about different import path

--- pylabianca/utils/_compat.py
+++ pylabianca/utils/_compat.py
@@ -1,15 +1,20 @@
 import warnings
 
 
+def _warn_different_import_path(old_path, new_path):
+    warnings.warn(f'`pylabianca.{old_path}` has moved to '
+                  f'`pylabianca.{new_path}`. The old import path '
+                  'will be removed in a future version.', FutureWarning,
+                  stacklevel=4)
+
+
 def dict_to_xarray(*args, **kwargs):
     '''This function has moved. Use ``pylabianca.analysis.dict_to_xarray``
     instead.'''
 
     # raise deprecation warning
-    warnings.warn('`pylabianca.utils.dict_to_xarray` has moved to '
-                  '`pylabianca.analysis.dict_to_xarray`. The old import path '
-                  'will be removed in a future version.', FutureWarning, stacklevel=3)
-
+    _warn_different_import_path('utils.dict_to_xarray',
+                                'analysis.dict_to_xarray')
     from ..analysis import dict_to_xarray as _dict_to_xarray
     return _dict_to_xarray(*args, **kwargs)
 
@@ -19,10 +24,8 @@ def xarray_to_dict(*args, **kwargs):
     instead.'''
 
     # raise deprecation warning
-    warnings.warn('`pylabianca.analysis.xarray_to_dict` has moved to '
-                  '`pylabianca.analysis.xarray_to_dict`. instead. The old '
-                  'import path will be removed in a future version.',
-                  FutureWarning, stacklevel=3)
+    _warn_different_import_path('utils.xarray_to_dict',
+                                'analysis.xarray_to_dict')
 
     from ..analysis import xarray_to_dict as _xarray_to_dict
     return _xarray_to_dict(*args, **kwargs)
@@ -33,10 +36,8 @@ def spike_centered_windows(*args, **kwargs):
     ``pylabianca.analysis.spike_centered_windows`` instead.'''
 
     # raise deprecation warning
-    warnings.warn('`pylabianca.analysis.spike_centered_windows` has moved to '
-                  '`pylabianca.analysis.spike_centered_windows`. instead. The old '
-                  'import path will be removed in a future version.',
-                  FutureWarning, stacklevel=3)
+    _warn_different_import_path('utils.spike_centered_windows',
+                                'analysis.spike_centered_windows')
 
     from ..analysis import spike_centered_windows as _spike_centered_windows
     return _spike_centered_windows(*args, **kwargs)
@@ -47,10 +48,8 @@ def shuffle_trials(*args, **kwargs):
     instead.'''
 
     # raise deprecation warning
-    warnings.warn('`pylabianca.analysis.shuffle_trials` has moved to '
-                  '`pylabianca.analysis.shuffle_trials`. instead. The old '
-                  'import path will be removed in a future version.',
-                  FutureWarning, stacklevel=3)
+    _warn_different_import_path('utils.shuffle_trials',
+                                'analysis.shuffle_trials')
 
     from ..analysis import shuffle_trials as _shuffle_trials
     return _shuffle_trials(*args, **kwargs)
@@ -60,11 +59,8 @@ def read_drop_info(*args, **kwargs):
     '''This function has moved. Use ``pylabianca.io.read_drop_info`` instead.'''
 
     # raise deprecation warning
-    warnings.warn('`pylabianca.analysis.read_drop_info` has moved to '
-                  '`pylabianca.analysis.read_drop_info`. instead. The old '
-                  'import path will be removed in a future version.',
-                  FutureWarning, stacklevel=3)
-
+    _warn_different_import_path('utils.read_drop_info',
+                                'analysis.read_drop_info')
 
     from ..io import read_drop_info as _read_drop_info
     return _read_drop_info(*args, **kwargs)
